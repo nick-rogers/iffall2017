39255d24c00a39578b5862a157baf0c784803967
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2017-12-06 07:56:56 -0600

    Final Page Upload

---
+++ microsite/finished/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	var section = document.getElementById("para");
+	section.scrollTop = section.scrollHeight;
+
+	//window.scrollTo(0,document.querySelector(".parallax-back").scrollHeight);
+
+	/*var isGrey = true;
+
+	$('.main').click(function(){
+
+	if(isGrey){
+		$(this).css('background-color', 'red');
+		isGrey = false;
+	} else {
+		$(this).css('background-color', 'lightGrey');
+		isGrey = true;
+	}
+
+	});
+	*/
+
+
+
+});
